46e8a06f36a243b4d1765e1bf4aecd9cb4b39fd2
Max <forsman.max@gmail.com>
2021-09-27 20:22:54 +0200

    Plot created auctions also

--- explorer.py
+++ explorer.py
@@ -10,19 +10,32 @@ def load_data(path: str):
     return df
 
 
-@click.command()
-@click.argument("data_path")
-def scatter(data_path):
-    data = load_data(data_path)
-    price = "total_price"
-    data["created_date"] = pd.to_datetime(data['created_date'])
-    data[price] = data[price] / 1e18
-    filtered = data.query(f"{price} < 10")
-    filtered = filtered.query("created_date > 20210923")
-    = sb.scatterplot(data=filtered, x="created_date", y=price)
+def plot_stuff():
+    successful = load_data("./data/filtered/successful.json")
+    created = load_data("./data/filtered/created.json")
+    successful["created_date"] = pd.to_datetime(successful['created_date'])
+    successful["total_price"] = successful["total_price"] / 1e18
+
+    created["created_date"] = pd.to_datetime(created['created_date'])
+    created["ending_price"] = created["ending_price"] / 1e18
+
+    successful = successful.query("total_price < 10")
+    successful = successful.query("created_date > 20210923")
+
+    created = created.query("ending_price < 10")
+    created = created.query("created_date > 20210923")
+    
+    sb.scatterplot(data=successful, x="created_date", y="total_price")
+    sb.scatterplot(data=created, x="created_date", y="ending_price")
 
     plt.xticks(rotation=45)
     plt.show(block=True)
+
+
+@click.command()
+@click.argument("data_path")
+def scatter(data_path):
+    plot_stuff()
 
 
 @click.group()
