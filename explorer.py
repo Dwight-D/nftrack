47a4f95ad2a58a98bf1ed684f2edc6420a563add
Max <forsman.max@gmail.com>
2021-09-27 21:38:33 +0200

    Disable auction plot, tweak filter

--- explorer.py
+++ explorer.py
@@ -19,14 +19,14 @@ def plot_stuff():
     created["created_date"] = pd.to_datetime(created['created_date'])
     created["ending_price"] = created["ending_price"] / 1e18
 
-    successful = successful.query("total_price < 10")
+    successful = successful.query("total_price < 5")
     successful = successful.query("created_date > 20210923")
 
     created = created.query("ending_price < 10")
     created = created.query("created_date > 20210923")
     
     sb.scatterplot(data=successful, x="created_date", y="total_price")
-    sb.scatterplot(data=created, x="created_date", y="ending_price")
+    #sb.scatterplot(data=created, x="created_date", y="ending_price")
 
     plt.xticks(rotation=45)
     plt.show(block=True)
--- filter.sh
+++ filter.sh
@@ -6,11 +6,11 @@ NFTRACK_FILTER_DIR=$NFTRACK_DATA_DIR/filtered
 
 filter_successful() {
     local infile=$1
-    cat $infile | sed 's/,$//' | jq '{total_price, created_date}'
+    cat $infile | sed 's/,$//' | jq '{total_price, created_date, id}'
 }
 
 filter_created() {
     local infile=$1
-    cat $infile | sed 's/,$//' | jq '{ending_price, created_date}'
+    cat $infile | sed 's/,$//' | jq '{ending_price, created_date, id}'
 }
 
